5d4b62e70982eb39807d1f6146dfee7a440e9139
darconso.davide <davide.darconso@edu.iisleviponti.it>
2024-03-04 13:25:14 +0100

    fun computer

--- app/src/main/java/com/example/paperrockscissor/MainActivity.kt
+++ app/src/main/java/com/example/paperrockscissor/MainActivity.kt
@@ -3,14 +3,25 @@ package com.example.paperrockscissor
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.paperrockscissor.ui.theme.PaperRockScissorTheme
 
@@ -20,8 +31,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             PaperRockScissorTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                )
+                {
+                  ButtonsTxt()
                 }
             }
         }
@@ -29,20 +44,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column{
-        Text(
-        text = "Rock, Paper, Scissor",
-        fontSize = 100.sp,
-        modifier = modifier)
+fun ButtonsTxt() {
+
+    var number by remember {
+        mutableStateOf(0)
     }
-//.9-.o-o
+
+    var pcNumber by remember {
+        mutableStateOf(0)
+    }
+
+    var point by remember {
+        mutableStateOf(0)
+    }
+
+    var pcPoint by remember {
+        mutableStateOf(0)
+    }
+
+    var text by remember {
+        mutableStateOf("")
+    }
+
+    var win by remember {
+        mutableStateOf(0)
+    }
+
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Row(
+            Modifier.padding(bottom = 16.dp)
+        ) {
+            Button(
+                onClick = { number = 0
+                    pcNumber = computer();
+                       if(number == 0 && pcNumber == 1 || number == 1 && pcNumber == 2 || number == 2 && pcNumber == 0)
+                       {
+                           win = 0
+                           pcPoint++
+                       }
+                    if(number == 1 && pcNumber == 0 || number == 2 && pcNumber == 1 || number == 0 && pcNumber == 2)
+                    {
+                        win = 1
+                        point++
+                    }
+            }) {
+
+            }
+        }
+    }
+
 }
+
+fun computer(): Int{
+    return(0..2).random()
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    PaperRockScissorTheme {
-        Greeting("Android")
-    }
+
 }
